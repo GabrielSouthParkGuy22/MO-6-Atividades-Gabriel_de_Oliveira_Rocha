e2e8e7a14a8d202c428427eac3d0d7554e97d806
GabrielSouthParkGuy22 <145403361+GabrielSouthParkGuy22@users.noreply.github.com.>
2023-11-22 22:19:56 -0300

    atividade do dia 22/11/2023

---
+++ 1primeira_semana/22_11_2023/script.js
@@ -0,0 +1,96 @@
+const root_container = document.querySelector("#root");
+
+function UserForm() {
+  return (
+    <form action="" id="user-form">
+      <div id="userDataContainer">
+        <h1 id="userData">Dados Cadastrais</h1>
+      </div>
+      <div id="form-container">
+        <div className="item1">
+          <label className="label-tag" for="name">
+            Nome completo:
+          </label>
+          <input className="input-field" type="text" id="name" name="name" />
+        </div>
+
+        <div className="item2">
+          <label className="label-tag" for="e-mail">
+            Email:
+          </label>
+          <input
+            className="input-field"
+            type="email"
+            id="e-mail"
+            name="e-mail"
+          />
+        </div>
+
+        <div className="item3">
+          <label className="label-tag" for="phone">
+            Telefone:
+          </label>
+          <input className="input-field" type="tel" id="phone" name="phone" />
+        </div>
+
+        <div className="item4">
+          <label className="label-tag" for="userDate">
+            Data de Nacimento
+          </label>
+          <input
+            className="input-field"
+            type="date"
+            name="userDate"
+            id="userDate"
+          />
+        </div>
+
+        <div className="item5">
+          <label className="label-tag" for="userCPF">
+            CPF
+          </label>
+          <input
+            className="input-field"
+            type="text"
+            name="userCPF"
+            id="userCPF"
+          />
+        </div>
+
+        <div className="item6">
+          <label className="label-tag" for="userCell">
+            Celular
+          </label>
+          <input
+            className="input-field"
+            type="tel"
+            name="userCell"
+            id="userCell"
+          />
+        </div>
+
+        <div className="item7">
+          <label className="label-tag" for="pw">
+            Senha:
+          </label>
+          <input className="input-field" type="password" id="pw" name="pw" />
+        </div>
+
+        <div className="item8">
+          <label className="label-tag" for="checkPW">
+            Confirmar Senha:
+          </label>
+          <input
+            className="input-field"
+            type="password"
+            id="checkPW"
+            name="checkPW"
+          />
+        </div>
+      </div>
+      <button id="form-btn">CONFIRMAR</button>
+    </form>
+  );
+}
+
+ReactDOM.render(<UserForm />, root_container);
